e21f1401eb7f79e2ead307d3ae3e530799800bec
Aindri Mukherjee <aindri2015@gmail.com>
2021-02-07 12:33:58 +0530

    Added Fullscreen support

--- app/src/main/java/com/aindri/calculator/CalculationSecond.java
+++ app/src/main/java/com/aindri/calculator/CalculationSecond.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -17,6 +18,7 @@ public class CalculationSecond extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_calculation_second);
         edt_num1=findViewById(R.id.edt_num1);
         edt_num2=findViewById(R.id.edt_num2);
