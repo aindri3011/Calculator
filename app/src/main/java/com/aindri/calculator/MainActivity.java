2c5f7bce343e71bd6ce05233cbe28a658e770f6b
Aindri Mukherjee <aindri2015@gmail.com>
2021-02-07 12:33:21 +0530

    Added Fullscreen support

--- app/src/main/java/com/aindri/calculator/MainActivity.java
+++ app/src/main/java/com/aindri/calculator/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Telephony;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         declareView();
         Bundle bundle = getIntent().getExtras();
